9975f302c95cfea2f811a489211f8b2381991135
Juanlu <juanlurasgue06@gmail.com>
2025-09-28 09:48:14 +0200

    First commit

---
+++ src/products/products.service.ts
@@ -0,0 +1,75 @@
+import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaClient } from 'generated/prisma';
+import { PaginationDto } from 'src/common';
+
+@Injectable()
+export class ProductsService extends PrismaClient implements OnModuleInit {
+
+  private readonly logger = new Logger(ProductsService.name);
+
+  async onModuleInit() {
+    await this.$connect();
+    this.logger.log('Database connected');
+  }
+  create(createProductDto: CreateProductDto) {
+    return this.product.create({data: createProductDto});
+  }
+
+  async findAll(paginarionDto: PaginationDto) {
+    const { limit = 10, page = 1 } = paginarionDto;
+    const totalPages =  await this.product.count({
+      where: { available: true }
+    });
+    const lastPage = Math.ceil(totalPages / limit);
+    return {
+      data: await this.product.findMany({
+        where: { available: true }, 
+          'take': limit,
+          'skip': (page - 1) * limit  
+        }),
+      meta:{
+        total: totalPages,
+        page,
+        lastPage
+      },
+
+  }
+  }
+
+  async findOne(id: number) {
+    const product = await this.product.findUnique({
+      where: { id, available: true }
+    });
+
+    if(!product){
+      throw new NotFoundException(`Product with id ${id} not found`);
+    }
+    return product
+  }
+
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    const { id: _, ...data } = updateProductDto;
+
+    await this.findOne(id);
+
+    return await this.product.update({
+      where: { id },
+      data
+    });
+  }
+
+  async remove(id: number) {
+    await this.findOne(id);
+
+    /*return await this.product.delete({
+      where: { id }
+      });*/
+    const product = await this.product.update({
+      where: { id },
+      data: { available: false }
+    });
+    return product;
+  }
+}
